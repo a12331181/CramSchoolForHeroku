e4d730157804fe452ed19c727c35c3865aac1678
a12331181 <a12331181@gmail.com>
2021-08-10 11:17:30 +0800

    Add admin cramschool index page

---
+++ controllers/adminController.js
@@ -0,0 +1,7 @@
+const adminController = {
+  getSchoolIndexPage: (req, res) => {
+    return res.render('admin/cramschool')
+  }
+}
+
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const schoolController = require('../controllers/schoolController.js')
+const adminController = require('../controllers/adminController.js')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/cramschool'))
   app.get('/cramschool', schoolController.getSchoolIndexPage)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/cramschool'))
+  app.get('/admin/cramschool', adminController.getSchoolIndexPage)
 }
