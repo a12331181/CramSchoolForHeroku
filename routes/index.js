df9b6c2e8f491a2dc3d71c1074544ab227dd6734
a12331181 <a12331181@gmail.com>
2021-08-10 13:14:03 +0800

    Add userController and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const schoolController = require('../controllers/schoolController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/cramschool'))
@@ -7,4 +8,7 @@ module.exports = app => {
 
   app.get('/admin', (req, res) => res.redirect('/admin/cramschool'))
   app.get('/admin/cramschool', adminController.getSchoolIndexPage)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
