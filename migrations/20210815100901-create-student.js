8167e8166ce3b20d624daf12bb486f0f466098b0
a12331181 <a12331181@gmail.com>
2021-08-15 18:12:22 +0800

    Create student model

---
+++ migrations/20210815100901-create-student.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      sex: {
+        type: Sequelize.STRING
+      },
+      birth: {
+        type: Sequelize.STRING
+      },
+      school: {
+        type: Sequelize.STRING
+      },
+      grade: {
+        type: Sequelize.STRING
+      },
+      tel: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Students');
+  }
+};
