ce7dec854dfd25eaf1e939e97c9f1dd452d34c1f
a12331181 <a12331181@gmail.com>
2021-09-16 10:38:49 +0800

    Create payment model & association between enrollment

---
+++ migrations/20210916022737-create-payment.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      time: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      isPaid: {
+        type: Sequelize.BOOLEAN
+      },
+      EnrollmentId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Payments');
+  }
+};
---
+++ models/payment.js
@@ -0,0 +1,21 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Payment extends Model {
+    static associate(models) {
+      Payment.belongsTo(models.Enrollment)
+    }
+  };
+  Payment.init({
+    time: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    isPaid: DataTypes.BOOLEAN,
+    EnrollmentId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Payment',
+  });
+  return Payment;
+};
