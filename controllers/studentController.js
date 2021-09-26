63b6d236b3341d513a58e7dda1094e3fe14f32e6
a12331181 <a12331181@gmail.com>
2021-09-26 16:38:08 +0800

    Add pagination feature in get students page

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,25 +1,77 @@
 const db = require('../models')
 const Student = db.Student
 const Course = db.Course
+const pageLimit = 10
 
 const studentController = {
   getStudents: (req, res) => {
+    let offset = 0
+    let isFilter = false
     const whereQuery = {}
-    if (req.query.CourseId) {
-      whereQuery.id = Number(req.query.CourseId)
+    let courseId = ''
+    if (req.query.page) {
+      offset = (Number(req.query.page) - 1) * pageLimit
     }
-    Student.findAll({
-      raw: true, 
-      nest: true,
-      include: [{ model: Course, as: 'EnrolledCourses', where: whereQuery }]
-    }).then(students => {
-      Course.findAll({
+    if (req.query.courseId) {
+      courseId = Number(req.query.courseId)
+      whereQuery.id = courseId
+      Student.findAndCountAll({
         raw: true,
-        nest: true
-      }).then(courses => {
-        return res.render('students', { students: students, courses: courses, courseId: whereQuery.id })
+        nest: true,
+        include: [{ model: Course, as: 'EnrolledCourses', where: whereQuery }],
+        limit: pageLimit,
+        offset: offset
+      }).then(result => {
+        const page = Number(req.query.page) || 1
+        const pages = Math.ceil(result.count / pageLimit)
+        const totalPage = Array.from({ length: pages }).map((v, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? pages : page + 1
+        isFilter = true
+        Course.findAll({
+          raw: true,
+          nest: true
+        }).then(courses => {
+          return res.render('students', { 
+            students: result.rows,
+            courses: courses,
+            courseId: whereQuery.id,
+            isFilter: isFilter,
+            page: page,
+            totalPage: totalPage,
+            prev: prev,
+            next: next
+          })
+        })
       })
-    })
+    } else {
+      Student.findAndCountAll({
+        raw: true,
+        nest: true,
+        limit: pageLimit,
+        offset: offset
+      }).then(result => {
+        const page = Number(req.query.page) || 1
+        const pages = Math.ceil(result.count / pageLimit)
+        const totalPage = Array.from({ length: pages }).map((v, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? pages : page + 1
+        Course.findAll({
+          raw: true,
+          nest: true
+        }).then(courses => {
+          return res.render('students', { 
+            students: result.rows,
+            courses: courses,
+            isFilter: isFilter,
+            page: page,
+            totalPage: totalPage,
+            prev: prev,
+            next: next
+          })
+        })
+      })
+    }
   },
 
   getStudent: (req, res) => {
